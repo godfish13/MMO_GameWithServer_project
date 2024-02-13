ab70cdac876c0bf19269c09ca39866e6967ce71e
godfish13 <63995325+godfish13@users.noreply.github.com>
2024-02-14 08:07:55 +0900

    20240214_PlayerCtrl
    이동 및 animation ctrl 구현

--- Client/Assets/Scripts/Controllers/PlayerCtrl.cs
+++ Client/Assets/Scripts/Controllers/PlayerCtrl.cs
@@ -1,128 +1,155 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Define;
 
 public class PlayerCtrl : MonoBehaviour
 {
     //GameObject (Player) 부착
 
-    [SerializeField] float M_speed = 10.0f;
-    Vector3 MouseClickDestination;
+    public Grid _grid;
+    public float _speed = 5.0f;
 
-    public enum PlayerStatue
+    [SerializeField] private Vector3Int _cellPos = Vector3Int.zero;
+    private bool _isMoving = false;
+    Animator _animator;
+
+    private MoveDir _dir = MoveDir.Down;    // 초기상태가 앞을 바라보는 상태로 설정
+    public MoveDir Dir      // 현재 상태 설정과 애니메이션을 동시에 변경되도록 프로퍼티 설정
     {
-        Die,
-        Moving,
-        Idle,
+        get { return _dir; }
+        set
+        {
+            if (_dir == value)
+                return;
+            switch (value) 
+            {
+                case MoveDir.Up:
+                    _animator.Play("WALK_BACK");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Down:
+                    _animator.Play("WALK_FRONT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Right:
+                    _animator.Play("WALK_RIGHT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Left:
+                    _animator.Play("WALK_RIGHT");
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);  // localScale x값 -1로 넣으면 좌우반전됨
+                    break;
+                case MoveDir.None:
+                    if (_dir == MoveDir.Up)
+                    {
+                        _animator.Play("IDLE_BACK");
+                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
+                    else if (_dir == MoveDir.Right)
+                    {
+                        _animator.Play("IDLE_RIGHT");
+                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
+                    else if (_dir == MoveDir.Left)
+                    {
+                        _animator.Play("IDLE_RIGHT");
+                        transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    }
+                    else                                // default 상태도 FRONT로 설정
+                    {
+                        _animator.Play("IDLE_FRONT");
+                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
+                    break;
+            }
+            _dir = value;
+        }
     }
-    [SerializeField] PlayerStatue _Statue = PlayerStatue.Idle;
 
     private void Start()
     {
-        /*Managers.inputMgr.KeyAction -= Move; // 다른부분에서 Move가 연동되있을시 액션이벤트가 여러번 발생하는 버그방지를 위해 초기화
-        Managers.inputMgr.KeyAction += Move; // inputMgr의 KeyAction에 Move 연동*/ // 본작에서는 일단 마우스 조종만 구현
-
-        Managers.inputMgr.MouseAction -= OnMouseClicked;
-        Managers.inputMgr.MouseAction += OnMouseClicked;
+        Vector3 pos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);    // CellToWorld : 셀 좌표를 월드좌표로 변환해줌
+        transform.position = pos;
+        _animator = gameObject.GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
-        switch (_Statue)
-        {
-            case PlayerStatue.Die:
-                UpdateDie();
-                break;
-            case PlayerStatue.Idle:
-                UpdateIdle();
-                break;
-            case PlayerStatue.Moving:
-                UpdateMoving();
-                break;
-        }    
+        GetDirInput();
+        UpdatePosition();
+        UpdateIsMoving();     
     }
 
-    void UpdateDie()
-    {
-        // 아무것도 못하는 사망상태
-    }
-
-    void UpdateMoving()
-    {
-        Vector3 dir = MouseClickDestination - transform.position;
-        if (dir.magnitude < 0.0001f)
-        {
-            _Statue = PlayerStatue.Idle;
-        }
-        else
-        {
-            float MoveDist = Mathf.Clamp(M_speed * Time.deltaTime, 0, dir.magnitude);
-            // Clamp(float value, min, max) : value값이 min과 max 범위 이외의 값을 넘지 않도록 함 -> 정확히 이동할 거리 계산
-            transform.position += dir.normalized * MoveDist;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
-        }
-
-        // 이하 애니메이션 처리
-        Animator Anim = GetComponent<Animator>();
-        // 현재 게임 상태에 대한 정보를 넘겨줌
-        Anim.SetFloat("Speed", M_speed);
-    }
-
-    /*void OnRunEvent()     // AnimationEvent 실험
-    {
-        Debug.Log("뚜벅뚜벅")
-    }*/
-
-    void UpdateIdle()
-    {
-
-        // 이하 애니메이션 처리
-        Animator Anim = GetComponent<Animator>();
-        Anim.SetFloat("Speed", 0);
-    }
-
- /*   void Move()
+    void GetDirInput()  // 방향 설정
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.1f);
-            transform.position += Vector3.forward * Time.deltaTime * M_speed;
+            Dir = MoveDir.Up;
         }
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.1f);
-            transform.position += Vector3.back * Time.deltaTime * M_speed;
+            Dir = MoveDir.Down;
         }
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.1f);
-            transform.position += Vector3.left * Time.deltaTime * M_speed;
+            Dir = MoveDir.Left;
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.1f);
-            transform.position += Vector3.right * Time.deltaTime * M_speed;
+            Dir = MoveDir.Right;
         }
-    }*/
+        else
+        {
+            Dir = MoveDir.None;
+        }
+    }
 
-    void OnMouseClicked(Define.MouseEvent evt)
+    void UpdatePosition()   // Grid 한칸씩 이동하도록 구현
     {
-        /*if (evt != Define.MouseEvent.Click)     // 마우스 클릭하고 뗏을때만 작동하도록 함, inputMgr의 Mouse클릭 이벤트 액션 호출 참조
-            return;*/
-
-        if (_Statue == PlayerStatue.Die)
+        if (_isMoving == false)
             return;
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+        Vector3 destpos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);    // CellToWorld : 셀 좌표를 월드좌표로 변환해줌
+        Vector3 moveDir = destpos - transform.position;
 
-        LayerMask mask = LayerMask.GetMask("Wall");
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100.0f, mask))
+        // 도착 여부 체크
+        float dist = moveDir.magnitude;
+        if (dist < _speed * Time.deltaTime) // 이동거리가 한번에 이동 가능한 거리보다 짧은경우 도착했다고 인정
         {
-            MouseClickDestination = hit.point;
-            _Statue = PlayerStatue.Moving;
-            //Debug.Log($"RayCast Camera : {hit.collider.gameObject.name}");
+            transform.position = destpos;
+            _isMoving = false;
+        }
+        else
+        {
+            transform.position += moveDir.normalized * _speed * Time.deltaTime;
+            _isMoving = true;
+        }
+    }
+
+    void UpdateIsMoving()   // Int 크기로 player 위치 변경
+    {
+        if (_isMoving == false)     // 1칸씩만 움직이고 멈추도록 bool조건 추가
+        {
+            switch (_dir)
+            {
+                case MoveDir.Up:      
+                    _cellPos += Vector3Int.up;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Down:
+                    _cellPos += Vector3Int.down;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Left:
+                    _cellPos += Vector3Int.left;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Right:
+                    _cellPos += Vector3Int.right;
+                    _isMoving = true;
+                    break;
+            }
         }
     }
 }
